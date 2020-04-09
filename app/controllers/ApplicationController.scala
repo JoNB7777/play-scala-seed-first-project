8da4b374f1f3f01b3b2fe45bb7ed334604371ff6
Johannes Bull <johannesbull@macbook-pro.lan>
2020-04-09 16:41:02 +0100

    working on part 2

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,17 +1,27 @@
 package controllers
 
 import javax.inject.Inject
+import models.DataModel
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import reactivemongo.core.errors.DatabaseException
+import repositories.DataRepository
 
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+import scala.concurrent.{ExecutionContext, Future}
 
-  def index(): Action[AnyContent] = TODO
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository, implicit val ec: ExecutionContext) extends BaseController {
 
-  def create(): Action[AnyContent] = TODO
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.find().map(items => Ok(Json.toJson(items)))
+  }
+
+  def create = TODO
 
   def read(id: String): Action[AnyContent] = TODO
 
   def update(id: String): Action[AnyContent] = TODO
 
-  def delete(id: String): Action[AnyContent] = TODO
+  def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.delete(id).map(_ => Status(ACCEPTED))
+  }
 }
--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -3,17 +3,29 @@ package controllers
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.mvc.ControllerComponents
 import uk.gov.hmrc.play.test.UnitSpec
+import play.api.test.FakeRequest
+import play.api.http.Status
+import repositories.DataRepository
+
+import scala.concurrent.ExecutionContext
 
 class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerSuite {
 
   val controllerComponents: ControllerComponents = app.injector.instanceOf[ControllerComponents]
+  val dataRepository: DataRepository = app.injector.instanceOf[DataRepository]
+  implicit val
 
   object TestApplicationController extends ApplicationController(
-    controllerComponents
+    controllerComponents, dataRepository, implicit val ec: ExecutionContext
   )
 
-  "ApplicationController .index()" should {
+  "ApplicationController .index" should {
 
+    val result = TestApplicationController.index()(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.OK
+    }
   }
 
   "ApplicationController .create()" should {
